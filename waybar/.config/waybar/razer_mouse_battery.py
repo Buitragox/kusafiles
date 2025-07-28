521340816fac4d38c5c8b5eebcdfc4b8044b8e70
Jhoan Buitrago <jhoanuitrago@gmail.com>
2025-07-27 19:16:52 -0500

    Add error handling to razer battery script

--- waybar/.config/waybar/razer_mouse_battery.py
+++ waybar/.config/waybar/razer_mouse_battery.py
@@ -1,28 +1,40 @@
+import logging
 from openrazer.client import DeviceManager
+from openrazer.client import DaemonNotFound
 from openrazer.client.devices import RazerDevice
 from json import dumps
 from time import sleep
 from sys import stdout
 
+logger = logging.getLogger("MouseBattery")
+handler = logging.StreamHandler()
+formatter = logging.Formatter("{asctime} - {name} - {levelname} - {message}", style='{', datefmt="%Y-%m-%d %H:%M:%S")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
 def mouse_info() -> dict:
-    device_manager = DeviceManager()
-    devices = list(filter(lambda d: d.type == 'mouse', device_manager.devices))
+    try:
+        device_manager = DeviceManager()
+        devices = list(filter(lambda d: d.type == 'mouse', device_manager.devices))
 
-    if not devices:
+        if not devices:
+            return {"percentage": 0}
+
+        device: RazerDevice = devices[0]
+        battery = device.battery_level
+
+        res = {"percentage": battery}
+
+        if device.is_charging:
+            res["class"] = "charging"
+            res["alt"] = "charging"
+        elif battery < 15:
+            res["class"] = "low"
+
+        return res
+    except DaemonNotFound:
+        logger.error("OpenRazer Daemon not found.")
         return {"percentage": 0}
-
-    device: RazerDevice = devices[0]
-    battery = device.battery_level
-
-    res = {"percentage": battery}
-
-    if device.is_charging:
-        res["class"] = "charging"
-        res["alt"] = "charging"
-    elif battery < 15:
-        res["class"] = "low"
-
-    return res
 
 
 def main():
