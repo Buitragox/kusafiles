d546045e428a50eca1b59c8f5fa521950dddad4d
Jhoan Buitrago <jhoanuitrago@gmail.com>
2025-08-18 14:43:08 -0500

    Add install.sh for stow packages and grub theme

---
+++ install.sh
@@ -0,0 +1,13 @@
+# Check if stow is installed
+if ! command -v stow >/dev/null 2>&1; then
+  echo "stow is not installed. Install it with:\nsudo pacman -Sy stow"
+fi
+
+# Standard stuff
+stow atuin delta hypr kitty nvim rofi waybar zsh
+
+# Packages that work outside of home directory
+sudo stow grub sddm -t /
+
+# Can't create symlinks to /boot, so we copy paste
+sudo cp ./grubthemes/MilkGrub /boot/grub/themes
